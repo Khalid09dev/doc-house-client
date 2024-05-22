3e84daa159d0bbf3372894cde40cc244161bfd4f
khalid09dev <mdkhalidhossen@hotmail.com>
2024-05-22 19:15:52 +0600

    added left side of the signup form

--- src/Layout/Main/Main.jsx
+++ src/Layout/Main/Main.jsx
@@ -1,13 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from '../../Components/Navbar/Navbar';
 import Footer from '../../Components/Footer/Footer';
 
 const Main = () => {
+    const location = useLocation();
+    const hideNavbarFooter = location.pathname === '/signup' || location.pathname === '/login';
+
     return (
         <div>
-            <Navbar></Navbar>
+            {!hideNavbarFooter && <Navbar></Navbar>}
             <Outlet></Outlet>
-            <Footer></Footer>
+            {!hideNavbarFooter && <Footer></Footer>}
         </div>
     );
 };
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,8 +1,29 @@
+import {Link} from 'react-router-dom';
 
 const SignUp = () => {
+
     return (
-        <div>
-            <h3>sign up</h3>
+        <div className='flex h-screen'>
+            <div className='bg-[#07332F] bg-right-top relative flex flex-col items-center w-1/2 bg-no-repeat' style={{backgroundImage: `url('https://i.postimg.cc/MH5fD0xK/doctor-frame.png')`}}>
+                {/* <img className='h-[300px] w-[400] relative left-24' src="" alt="#" /> */}
+                <img className='w-[370px] h-[370px] mt-60 ' src="https://i.postimg.cc/htVVdnmr/doctor.png" alt="#" />
+            </div>
+            <div className='flex justify-center w-1/2'>
+                <form>
+                    <h3>Sign Up to Doc House</h3>
+
+                    <label>Name</label> <br />
+                    <input type="text" name="name" id="" placeholder="Enter your name"/> <br />
+                    <label>Username</label> <br />
+                    <input type="text" name="username" id="" placeholder="Enter your username"/> <br />
+                    <label>Email</label> <br />
+                    <input type="email" name="email" id="" placeholder="Enter your email"/> <br />
+                    <label>Password</label> <br />
+                    <input type="password" name="password" id="" placeholder="Enter your password"/> <br />
+                    <input type="submit" value="Create Account" />
+                <p>Already Registered? Go to <Link to="/login">SIGN IN</Link></p>
+                </form>
+            </div>
         </div>
     );
 };
