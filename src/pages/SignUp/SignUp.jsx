1c2741dbba3de86c547832bd6caf230eaa7807d3
khalid09dev <mdkhalidhossen@hotmail.com>
2024-05-22 20:20:09 +0600

    make signup responsive for mobile device

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,16 +1,20 @@
 import {Link} from 'react-router-dom';
 
 const SignUp = () => {
+    const handleSignup = (e) => {
+        e.preventDefault();
+        console.log('clicking')
+    }
 
     return (
-        <div className='flex h-screen'>
-            <div className='bg-[#07332F] bg-right-top relative flex flex-col items-center w-1/2 bg-no-repeat' style={{backgroundImage: `url('https://i.postimg.cc/MH5fD0xK/doctor-frame.png')`}}>
+        <div className='md:flex grid md:h-screen'>
+            <div className='bg-[#07332F] bg-right-top relative flex flex-col items-center md:w-1/2 bg-no-repeat' style={{backgroundImage: `url('https://i.postimg.cc/MH5fD0xK/doctor-frame.png')`}}>
 
                 <img className='w-[370px] h-[370px] mt-60 ' src="https://i.postimg.cc/htVVdnmr/doctor.png" alt="#" />
             </div>
-            <div className='flex justify-center items-center w-1/2'>
-                <form className='border p-[50px] rounded-lg'>
-                    <h3 className='text-[#0A0808] text-[20px] font-bold pb-8 text-center px-14'>Sign Up to Doc House</h3>
+            <div className='md:flex justify-center items-center md:w-1/2'>
+                <form onSubmit={handleSignup} className='border md:p-[50px] p-[30px] rounded-lg'>
+                    <h3 className='text-[#0A0808] text-[20px] font-bold md:pb-8 pb-4 text-center md:px-14'>Sign Up to Doc House</h3>
 
                     <label className='text-base font-semibold text-[#0A0808]'>Name</label> <br />
                     <input className='bg-[#F3F3F3] py-3 pl-3 w-full placeholder:text-[15px] rounded-lg mb-3' type="text" name="name" id="" placeholder="Enter your name"/> <br />
