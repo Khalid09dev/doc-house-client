2588691dddace8d190a54bc3116d3476b4df6cbb
khalid09dev <mdkhalidhossen@hotmail.com>
2024-05-22 21:45:13 +0600

    added error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,16 @@
+import {Link} from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h3>This is error page</h3>
+        <div className='flex justify-center items-center min-h-screen'>
+            <div>
+                <h2 className="text-[#0A0808] text-center text-[50px] font-bold">Sorry<span className="text-[40px]">,</span></h2>
+                <h3 className="text-[40px] font-normal text-[#6C6B6B] text-center">This page is not found.</h3>
+                <img src="https://i.postimg.cc/Qt9Jpv5m/404.png" alt="#" />
+                <div className='flex justify-center'>
+                    <Link className='text-white text-base rounded-lg font-semibold py-3 bg-[#F7A582] px-14' to="/">Back to home</Link>
+                </div>
+            </div>
         </div>
     );
 };
