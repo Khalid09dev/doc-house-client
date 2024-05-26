3179855d0dcc475225b86f09a83af5da0686ce3a
khalid09dev <mdkhalidhossen@hotmail.com>
2024-05-26 13:51:07 +0600

    added a responsive navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,30 @@
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>This is navbar</h1>
+        <div className="navbar md:flex md:justify-center flex justify-between bg-base-100">
+            <div className="navbar-start flex justify-between items-center w-full">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <NavLink><a>Home</a></NavLink>
+                        <NavLink><a>About</a></NavLink>
+                        <NavLink><a>Appointment</a></NavLink>
+                        <NavLink><a>Login</a></NavLink>
+                    </ul>
+                </div>
+                <Link to="/" className='flex items-center text-[25px] font-bold gap-1'><img className="h-[35px] w-[35px]" src="https://i.postimg.cc/rF9CzMLL/logo.png" alt="#" /><span className='text-[#F7A582]'>Doc</span><span className='text-[#FFF]'>House</span></Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 text-[#F3F3F3] text-[15px] font-medium gap-7">
+                    <NavLink><a>Home</a></NavLink>
+                    <NavLink><a>About</a></NavLink>
+                    <NavLink><a>Appointment</a></NavLink>
+                    <NavLink><a>Login</a></NavLink>
+                </ul>
+            </div>
         </div>
     );
 };
--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
-        <div className='flex justify-center items-center min-h-screen'>
+        <div className='flex justify-center items-center h-screen'>
             <div>
                 <h2 className="text-[#0A0808] text-center text-[50px] font-bold">Sorry<span className="text-[40px]">,</span></h2>
                 <h3 className="text-[40px] font-normal text-[#6C6B6B] text-center">This page is not found.</h3>
