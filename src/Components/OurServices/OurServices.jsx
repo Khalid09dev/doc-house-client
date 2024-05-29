d035730536517a6c37ebb764530c575c3841cc76
khalid09dev <mdkhalidhossen@hotmail.com>
2024-05-29 13:35:40 +0600

    maked our services section responsive for mobile

--- src/Components/OurServices/OurServices.jsx
+++ src/Components/OurServices/OurServices.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 
 const OurServices = () => {
-    const [activeTab, setActiveTab] = useState(2);
+    const [activeTab, setActiveTab] = useState(1);
     return (
-        <div className="flex justify-center gap-6 py-32 bg-[#FFF]">
-            <img src="https://i.postimg.cc/J7BWrpxx/our-services-1.png" alt="#" />
+        <div className="md:flex justify-center gap-6 md:py-32 py-6 bg-[#FFF]">
+            <img className="px-3" src="https://i.postimg.cc/J7BWrpxx/our-services-1.png" alt="#" />
             <div>
-                <h2 className="text-[#0A0808] text-[40px] font-bold">Our Services</h2>
-                <p className="text-[#3B3A3A] text-base font-normal">Sed ut perspiciatis unde omnis iste natus error sit voluptatem <br /> accusantium doloremque laudantium, totam rem aperiam, <br /> eaque ipsa quae ab illo inve ntore veritatis et quasi <br /> architecto beatae vitae dicta sunt explicabo.</p>
+                <h2 className="text-[#0A0808] text-[40px] font-bold md:text-left text-center">Our Services</h2>
+                <p className="text-[#3B3A3A] text-base md:block hidden md:text-left font-normal">Sed ut perspiciatis unde omnis iste natus error sit voluptatem <br /> accusantium doloremque laudantium, totam rem aperiam, <br /> eaque ipsa quae ab illo inve ntore veritatis et quasi <br /> architecto beatae vitae dicta sunt explicabo.</p>
+                <p className="text-[#3B3A3A] text-base text-center md:hidden font-normal">Sed ut perspiciatis unde omnis iste natus error sit voluptatem  accusantium doloremque laudantium, totam rem aperiam,  eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
 
-            <div className="container mx-auto py-4">
+            <div className="container md:block hidden mx-auto py-4">
                 <div role="tablist" className="tabs flex items-center ">
                     <button
                         role="tab"
@@ -46,6 +47,38 @@ const OurServices = () => {
                     {activeTab === 3 && <div id="tab-content-3">Content for Tab 3</div>}
                 </div>
             </div>
+
+            {/* for mobile devices */}
+            <div className="container md:hidden mx-auto py-4">
+                <div role="tablist" className="tabs flex justify-center items-center ">
+                    <button
+                        role="tab"
+                        className={`tab ${activeTab === 1 ? 'tab-active' : ''} rounded-md bg-[#F7A582] py-[30px] px-7 text-[#3B3A3A] text-[18px] font-semibold`}
+                        onClick={() => setActiveTab(1)} style={{display: "inline-flex", justifyContent: "center", alignContent: "center"}} 
+                    >
+                        Gastrology
+                    </button>
+                    <button
+                        role="tab"
+                        className={`tab ${activeTab === 2 ? 'tab-active' : ''} border-l-0  border-[2px] py-7 px-7 text-[#3B3A3A] text-[18px] font-semibold`}
+                        onClick={() => setActiveTab(2)} style={{display: "inline-flex", justifyContent: "center", alignContent: "center"}}
+                    >
+                        Orthopedic
+                    </button>
+                </div>
+
+                <div className="tab-contents mt-4">
+                    {activeTab === 1 && <div id="tab-content-1">
+                        <img className="mx-auto" src="https://i.postimg.cc/fyn4nhxM/gastrology.png" alt="#" />
+                        <h3 className="text-[#0A0808] text-[28px] text-center font-bold pt-3 ">Electro  Gastrology Therapy</h3>
+                        <p className="text-base font-normal text-[#3B3A3A] text-center px-4 pb-3 pt-4">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium  doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Sed ut perspiciatis unde omnis iste natus error </p>
+                        <p className="text-base font-normal text-center pb-3 px-4 text-[#3B3A3A]">Sit voluptatem accusantium doloremque laudantium, totam rem aperiam,eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae  dicta sunt explicabo.</p>
+                        <div className="flex justify-center"><button className="text-[#F7A582] text-xl font-bold border-[2px] py-3 px-6 border-[#F7A582] rounded-lg">More Details</button></div>
+                    </div>}
+                    {activeTab === 2 && <div id="tab-content-2">Content for Tab 2</div>}
+                </div>
+            </div>
+            {/* for mobile devices */}
             </div>
         </div>
     );
