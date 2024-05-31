ba61f668c1bf7c3a87f984076b01ea43064f1242
khalid09dev <mdkhalidhossen@hotmail.com>
2024-05-31 09:29:13 +0600

    added responsive design for mobile for my appoinment page

--- src/pages/MyAppoinment/MyAppoinment.jsx
+++ src/pages/MyAppoinment/MyAppoinment.jsx
@@ -6,22 +6,22 @@ const MyAppoinment = () => {
         <div>
             <div className='fixed bottom-0 top-0 right-0 left-0'><Navbar></Navbar></div>
             {/* dashboard sidebar */}
-            <div className='fixed bg-[#FFFFFF]  top-0 bottom-0 left-0 mt-[70px]'>
+            <div className='fixed md:block hidden bg-[#FFFFFF]  top-0 bottom-0 left-0 mt-[70px]'>
                 <ul>
                     <li className='pl-10 pt-5 pr-14 text-[#000000] text-[16px] font-normal'><NavLink>My Appointments</NavLink></li>
                 </ul>
             </div>
             
             {/* dashboard content */}
-            <div className='flex-1 pt-20 pl-52 min-h-screen bg-[#F1F5F9]'>
-                <div className='flex justify-around items-center'>
+            <div className='flex-1 pt-20 md:pl-52 min-h-screen bg-[#F1F5F9] md:block '>
+                <div className='flex md:pt-0 pt-4 justify-around items-center'>
                     <h3 className='text-[#000000] text-2xl font-normal'>My Appointment</h3>
                     <button className='text-[#000000] text-base font-normal py-3 px-3 border rounded-lg border-[#000000]'>MAY 10, 2022</button>
                 </div>
 
                 <table className='mt-6'>
                     <thead>
-                        <tr className='text-[#000000] py-3 rounded-t-lg text-base font-bold bg-[#E6E6E6] flex mx-36 gap-52'>
+                        <tr className='text-[#000000] py-3 rounded-t-lg text-base font-bold bg-[#E6E6E6] flex md:mx-36 mx-7 md:gap-52 gap-10'>
                             <th></th>
                             <th>Name</th>
                             <th>Service</th>
@@ -29,211 +29,7 @@ const MyAppoinment = () => {
                         </tr>
                     </thead>
                     <tbody>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
-                        <th>1</th>
-                        <td>John Doe</td>
-                        <td>Teeth Orthodontics</td>
-                        <td>08.30 AM - 09.00 AM</td>
-                    </tr>
-                    <tr className='flex border text-[#000000] text-base font-normal items-center mx-36 px-6 gap-40 bg-white py-3'>
+                    <tr className='flex border text-[#000000] text-base font-normal items-center md:mx-36 mx-7 px-6 md:gap-40 gap-4 bg-white py-3'>
                         <th>1</th>
                         <td>John Doe</td>
                         <td>Teeth Orthodontics</td>
